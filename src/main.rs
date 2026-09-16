b1cb623f3793692a030f84a49b0d4c3503902fa5
Richard Inocencio <150263396+rjinocencio@users.noreply.github.com>
2026-09-15 19:18:43 -0700

    feat(GTPS-3): model spacecraft state with a struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
+mod spacecraft;
+
+use crate::spacecraft::Spacecraft;
+
 fn main() {
-    let identifier = "SAT-001";
-    let mode = "NOMINAL";
-    let battery_voltage = 28.5;
-    let temperature = 68.0;
+    let spacecraft = Spacecraft {
+        identifier: String::from("SAT-001"),
+        mode: String::from("NOMINAL"),
+        battery_voltage: 28.5,
+        temperature: 68.0,
+        uptime: 2.0,
+    };
 
     println!("==GROUND TELEMETRY PROCESSING SYSTEM ==");
-    println!("Spacecraft Identifier: {}", identifier);
-    println!("Mode: {}", mode);
-    println!("Battery Level: {:.1}V", battery_voltage);
-    println!("Temperature: {:.1}F", temperature);
+    status(&spacecraft);
+    println!("{} is in {} mode", spacecraft.identifier, spacecraft.mode);
+}
+
+fn status(spacecraft: &Spacecraft) {
+    println!("Spacecraft Identifier: {}", spacecraft.identifier);
+    println!("Mode: {}", spacecraft.mode);
+    println!("Battery Level: {:.1}V", spacecraft.battery_voltage);
+    println!("Temperature: {:.1}F", spacecraft.temperature);
+    println!("Uptime: {:.1} hrs", spacecraft.uptime)
 }
---
+++ src/spacecraft.rs
@@ -0,0 +1,7 @@
+pub struct Spacecraft {
+    pub identifier: String,
+    pub mode: String,
+    pub battery_voltage: f64,
+    pub temperature: f64,
+    pub uptime: f64,
+}
